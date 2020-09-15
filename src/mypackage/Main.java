02e50c9f23061aa042bfdce240eaefd6d8ad384c
NickGBR <gubarenko.nikita27@gmail.com>
2020-09-15 15:44:54 +0800

    Response added.
    I finihsed server by adding method with gives file list when it gets "GET" request.

---
+++ src/mypackage/Main.java
@@ -0,0 +1,28 @@
+package mypackage;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Создаем сервер и передаем в него имя и порт.
+        HttpServer server=null;
+        try {
+            server = HttpServer.create(new InetSocketAddress("localhost", 8001), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        //Передаем настройки сервера и запускаем его.
+        server.createContext("/socket", new MyHttpHandler());
+        server.start();
+
+    }
+}
---
+++ src/mypackage/MyHttpHandler.java
@@ -0,0 +1,65 @@
+package mypackage;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyHttpHandler implements HttpHandler {
+
+    String response = "";
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        //Имя текущей директории
+        String FOLDEPATH = "mypackage";
+
+        String response;
+
+        //Получаем спимок файлов в директории
+        File dir = new File(getClass().getClassLoader().getResource(FOLDEPATH).getFile());
+        File[] files = dir.listFiles();
+
+        //Если тип запроса "GET" то передаем список файлов
+        if(exchange.getRequestMethod().equals("GET")) {
+            response = fileToString(files);
+            handleResponse(exchange,response);
+        }
+        System.out.println(exchange.getRequestMethod());
+    }
+
+    /**Преобразование имен файлов в строковую константу
+     *
+     * @param files
+     * @return
+     */
+    private String fileToString(File[] files){
+        response = "";
+        for(File file : files){
+            response = response+file.getName();
+            response = response +"\n";
+        }
+        return response;
+    }
+
+    /** Метод принимает строку, и передает ее в браузер.
+     *
+     * @param exchange
+     * @param response
+     * @throws IOException
+     */
+    private void handleResponse(HttpExchange exchange, String response) throws IOException {
+
+        OutputStream outputStream = exchange.getResponseBody();
+
+        exchange.sendResponseHeaders(200,response.length());
+
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+        outputStream.close();
+        exchange.close();
+    }
+}
